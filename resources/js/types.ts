b427bd042764e107ec70ac4c1a52b12bd2bb8bdd
EmilienKopp <emilien.kopp@gmail.com>
2024-01-22 01:55:41 +0900

    feat: frontend TS types - WIP

--- resources/js/types.ts
+++ resources/js/types.ts
@@ -1,5 +1,46 @@
 
 
+export type Activity = {
+    id: number,
+    user_id: number,
+    project_id: number,
+    task_category_id: number,
+    task_id?: number,
+    date: string | Date,
+    rate?: number,
+    duration: number,
+    project: Project,
+    task_category: TaskCategory,
+    dailyLog?: DailyLog,
+}
+
+export type Project = {
+    id: number,
+    name: string,
+    company_id: number,
+    user_id: number,
+    description: string,
+    start_date?: string | Date,
+    end_date?: string | Date,
+}
+
+export type TaskCategory = {
+    id: number,
+    name: string,
+    description?: string,
+    color?: string,
+}
+
+export type DailyLog = {
+    date: string | Date,
+    project_id: number,
+    project_name: string,
+    total_seconds: number,
+    user_id: number,
+    user_name: string,
+}
+
+
 export interface InertiaForm extends Partial<HTMLFormElement> {
     errors?: { [key: string]: string },
     [key: string]: any,
