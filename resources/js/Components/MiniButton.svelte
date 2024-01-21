1353cb51ed31bba58535ed71801acb7caf2a5fb9
EmilienKopp <emilien.kopp@gmail.com>
2024-01-22 01:57:48 +0900

    feat: Add "ghost" color option and support for anchor tags in MiniButton component

--- resources/js/Components/MiniButton.svelte
+++ resources/js/Components/MiniButton.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
     import { twMerge } from "tailwind-merge";
 
-    export let color: "blue" | "red" | "green" | "yellow" | "gray" | "indigo" | "purple" | "pink" | "orange" | "teal" | "cyan" | "white" | "black" = "blue";
+    export let color: "blue" | "red" | "green" | "yellow" | "gray" | "indigo" | "purple" | "pink" | "orange" | "teal" | "cyan" | "white" | "black" | "ghost" = "blue";
     export let type: "button" | "submit" | "reset" = "button";
+    export let href: string = "";
 
     const colors = {
         blue: "bg-blue-500 hover:bg-blue-600 text-white",
@@ -18,9 +19,19 @@
         cyan: "bg-cyan-500 hover:bg-cyan-600",
         white: "bg-white hover:bg-gray-200",
         black: "bg-black hover:bg-gray-800 text-white",
+        ghost: "bg-transparent border-gray-300 hover:bg-gray-200 text-gray-400 hover:text-gray-700",
     }
+
+    const css = twMerge("border rounded px-2 max-h-6 bg-slate-300 text-black capitalize",colors[color], $$restProps.class);
+    const {class: _, ...rest} = $$restProps;
 </script>
 
-<button {type} class={twMerge("border rounded px-2 max-h-6 bg-slate-300 text-black capitalize",colors[color])} on:click {...$$restProps}>
+{#if !href}
+<button {type} class={css} on:click {...rest}>
     <slot></slot>
 </button>
+{:else}
+<a role="button" {href} class={css} on:click {...rest}>
+        <slot></slot>
+</a>
+{/if}
