656b2f7cf3326b9ddaca0aa4af0b5c43917a5631
EmilienKopp <emilien.kopp@gmail.com>
2024-01-22 01:55:12 +0900

    feat: fetchactivities by dates

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BatchStoreActivityRequest;
 use App\Models\Activity;
 use App\Http\Requests\StoreActivityRequest;
 use App\Http\Requests\UpdateActivityRequest;
@@ -9,30 +10,41 @@ use App\Models\DailyLog;
 use Carbon\Carbon;
 use App\Models\Project;
 use App\Models\TaskCategory;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\App;
 
 class ActivityController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
+        $taskCategories = TaskCategory::all();
+        $projects = Project::all();
+
+        $date = $request->query('date') ?? Carbon::today()->format('Y-m-d');
+
         $activities = Activity::where('user_id', auth()->user()->id)
-            ->where('date', Carbon::today())
-            ->with('project')
+            ->whereYear('date', Carbon::parse($date)->year)
+            ->whereMonth('date', Carbon::parse($date)->month)
+            ->with('project', 'taskCategory')
             ->orderBy('id', 'asc')
             ->get()
             ->transform(function ($activity) {
                 $activity->dailyLog = $activity->dailyLog();
                 return $activity;
             });
-        $projects = Project::all();
+
         $dailyLogs = DailyLog::where('user_id', auth()->user()->id)
-            ->where('date', Carbon::today())
+            ->whereYear('date', Carbon::parse($date)->year)
+            ->whereMonth('date', Carbon::parse($date)->month)
+            ->orderBy('date', 'desc')
             ->get();
-        $taskCategories = TaskCategory::all();
-        return inertia('Activity/Index', compact('activities', 'projects', 'dailyLogs', 'taskCategories'));
+
+        return inertia('Activity/Index', compact('activities', 'projects', 'dailyLogs', 'taskCategories', 'date'));
     }
 
     /**
@@ -49,23 +61,51 @@ class ActivityController extends Controller
     public function store(StoreActivityRequest $request)
     {
         $validated = $request->validated();
-        Log::debug($validated);
         $activities = $validated['activities'];
-        Log::debug($activities);
         Activity::where('user_id', auth()->user()->id)
             ->where('project_id', $activities[0]['project_id'])
             ->where('date', Carbon::today())
             ->delete();
         Activity::upsert($activities, ['project_id', 'user_id', 'task_category_id', 'date']);
-        return to_route('activities.index');
+        return to_route('activities.show', ['date' => $validated['date']]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Activity $activity)
+    public function show(Request $request)
     {
-        //
+        $date = $request->query('date') ?? Carbon::today()->format('Y-m-d');
+
+        $projects = Project::all();
+        $taskCategories = TaskCategory::all()->transform(function ($taskCategory) {
+            $taskCategory->name = __($taskCategory->name);
+            return $taskCategory;
+        });
+
+        
+        $activities = Activity::where('user_id', auth()->user()->id)
+            ->where('date', $date)
+            ->with('project')
+            ->with('taskCategory')
+            ->orderBy('id', 'asc')
+            ->get()
+            ->transform(function ($activity) {
+                $activity->dailyLog = $activity->dailyLog();
+
+                Log::debug($activity->taskCategory);
+                if($activity->taskCategory) {
+                    $activity->taskCategory = __($activity->taskCategory["name"]);
+                }
+                return $activity;
+            });
+        
+        $dailyLogs = DailyLog::where('user_id', auth()->user()->id)
+            ->where('date', $date)
+            ->orderBy('date', 'desc')
+            ->get();
+        
+        return inertia('Activity/Daily/Index', compact('activities', 'projects', 'dailyLogs', 'taskCategories'));
     }
 
     /**
