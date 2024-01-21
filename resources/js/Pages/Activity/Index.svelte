aa5183e97deda4d82533def44ce5f4f14b58a7c2
EmilienKopp <emilien.kopp@gmail.com>
2024-01-22 01:58:28 +0900

    feat: Refactor Activity/Index.svelte component for display per month

--- resources/js/Pages/Activity/Index.svelte
+++ resources/js/Pages/Activity/Index.svelte
@@ -1,26 +1,25 @@
 <script lang="ts">
-  import DailyLogInputForm from './DailyLogInputForm.svelte';
 
+    import MonthTable from "$components/MonthTable.svelte";
     import AuthenticatedLayout from "$layouts/AuthenticatedLayout.svelte";
-    import Select from "$components/Select.svelte";
-    import { Duration } from "$lib/Duration";
-    import TextInput from "$components/TextInput.svelte";
-    import { Input } from "flowbite-svelte";
-    import DurationInput from "$components/DurationInput.svelte";
-    import { toast } from "$lib/stores";
+    import { groupBy } from "$lib/Array";
 
-    export let activities: any[];
-    export let dailyLogs: any[];
-    export let taskCategories: any[];
 
+    export let activities: any[] = [];
+    export let dailyLogs: any[] = [];
+    export let taskCategories: any[] = [];
+    export let date: Date = new Date();
+
+
+
+    $: logs = groupBy(activities, 'date');
+    $: console.log(date)
 </script>
 
 <AuthenticatedLayout>
-    <div class="grid grid-cols-2 gap-4">
-        {#each dailyLogs as log}
-            <DailyLogInputForm {log} {taskCategories} activities={
-                activities.filter(a => a.project_id == log.project_id)
-            } />
-        {/each}
+
+    <div class="w3/4 mx-auto">
+        <MonthTable headers={["Date", "Project", "Activity", "Duration"]} data={logs} {date} />
     </div>
 </AuthenticatedLayout>
+
