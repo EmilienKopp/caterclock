c9eb521da81b587ba2d9ba348e075523c946c49e
EmilienKopp <emilien.kopp@gmail.com>
2024-02-06 02:29:27 +0900

    feat: project Show page (WIP)

---
+++ resources/js/Pages/Project/Show.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import MiniButton from "$components/MiniButton.svelte";
+    import AuthenticatedLayout from "$layouts/AuthenticatedLayout.svelte";
+    import { fly } from "svelte/transition";
+    
+    
+    export let project: any;
+    export let auth: any;
+
+    let tabs: string[] = ["Overview","Notes", "Tasks", "Reports"];
+    let activeTab: string = "Overview";
+
+    
+</script>
+
+<AuthenticatedLayout>
+    <h1 class="w-full flex items-center justify-between">
+        {project.name}
+        {#if project.company}
+            @<strong>{project.company.name}</strong>
+        {:else if project.user_id == auth.user.id}
+            <MiniButton href="/projects/{project.id}/edit">Edit</MiniButton>
+        {/if}
+    </h1>
+
+    <div role="tablist" class="tabs tabs-bordered">
+        {#each tabs as tab}
+            <button on:click={() => activeTab = tab} role="tab" 
+                class:tab-active={activeTab == tab} class="tab">
+                {tab}
+            </button>
+        {/each}
+    </div>
+
+</AuthenticatedLayout>
