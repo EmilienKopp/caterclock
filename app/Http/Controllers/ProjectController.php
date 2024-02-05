7f47d6cc56a9a1afd99a67a17287d0d6322eec9a
EmilienKopp <emilien.kopp@gmail.com>
2024-02-06 02:20:29 +0900

    feat: projects CRUD

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -5,13 +5,23 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreProjectRequest;
 use Illuminate\Http\Request;
 use App\Models\Project;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::getOwnedByAuthUser();
+        $user = User::authenticated();
+        $projects = $user->getInvolvedProjects();
         return inertia('Project/Index', compact('projects'));
+    }
+
+    public function show(Project $project)
+    {
+        $project = Project::where('id',$project->id)->with(['timeLogs','company'])->first();
+
+        return inertia('Project/Show', compact('project'));
     }
 
     public function create()
@@ -25,4 +35,20 @@ class ProjectController extends Controller
         return to_route('projects.index');
     }
 
+    public function edit(Project $project)
+    {
+        return inertia('Project/Edit', compact('project'));
+    }
+
+    public function update(StoreProjectRequest $request, Project $project)
+    {
+        $project->update($request->validated());
+        return to_route('projects.index');
+    }
+
+    public function destroy(Project $project)
+    {
+        $project->delete();
+        return to_route('projects.index');
+    }
 }
