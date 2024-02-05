320fadf08de5e665a3cb87aa650fd1efdd79b272
EmilienKopp <emilien.kopp@gmail.com>
2024-02-06 02:25:00 +0900

    feat: Tabs component

---
+++ resources/js/Components/TabItem.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import { getContext, onMount } from "svelte";
+    import { writable } from "svelte/store";
+    import { fade, fly, slide } from "svelte/transition";
+
+    export let title: string = "Tab";
+    export let open: boolean = false;
+
+
+    const context = getContext("context") ?? {} as any;
+    let active = context.active ?? writable<HTMLElement>();
+
+    function init(node: HTMLElement) {
+        active.set(node);
+
+        const destroy = active.subscribe((elem: HTMLElement) => {
+            if (elem !== node) {
+                open = false;
+            }
+        });
+
+        return { destroy };
+    }
+
+</script>
+
+
+<button type="button" role="tab" class="tab" on:click={() => open = true} class:tab-active={open}>
+    <slot name="title">
+        {title}
+    </slot>
+    {#if open}
+        <div class="hidden">
+            <div use:init transition:slide={{duration: 300}} class="w-full flex flex-col">
+
+                <slot name="head"/>
+
+                <slot />
+            </div>
+        </div>
+    {/if}
+</button>
---
+++ resources/js/Components/TabLayout.svelte
@@ -0,0 +1,38 @@
+<script context="module" lang="ts">
+    import { writable, type Writable } from "svelte/store";
+    export interface TabInfo {
+        active: Writable<HTMLElement>;
+    }
+</script>
+
+<script lang="ts">
+    import { setContext } from "svelte";
+    import { twMerge } from "tailwind-merge";
+
+    export let contentClass: string = "";
+
+    const tablistClasses = "tabs tabs-bordered w-full";
+
+    const context = {
+        active: writable<HTMLElement>()
+    }
+
+    setContext("context", {
+        ...context
+    });
+
+    function init(node: HTMLElement) {
+        const destroy = context.active.subscribe((elem: HTMLElement) => {
+            if(elem) node.replaceChildren(elem);
+        });
+
+        return {
+            destroy
+        }
+    }
+</script>
+
+<div role="tablist" class={twMerge(tablistClasses,$$restProps.class)}>
+    <slot />
+</div>
+<div role="tabpanel" aria-labelledby="id-tab" class={twMerge("pt-3",$$restProps.class,contentClass)} use:init></div>
