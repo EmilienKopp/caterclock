a8273932f6844fb66fea585864fc639420505724
EmilienKopp <emilien.kopp@gmail.com>
2024-02-06 02:26:05 +0900

    chore: cleanup

--- resources/js/Pages/Activity/Index.svelte
+++ resources/js/Pages/Activity/Index.svelte
@@ -3,16 +3,14 @@
     import AuthenticatedLayout from "$layouts/AuthenticatedLayout.svelte";
     import { groupBy } from "$lib/Array";
 
-    export let activities: any[] = [];
     export let dailyLogs: any[] = [];
-    export let taskCategories: any[] = [];
     export let date: Date = new Date();
 
 </script>
 
 <AuthenticatedLayout>
     <div class="w3/4 mx-auto">
-        <MonthTable headers={["Date", "Project", "Activity", "Duration"]} data={dailyLogs} {date} {activities}/>
+        <MonthTable headers={["Date", "Project", "Activity", "Duration"]} data={dailyLogs} {date} />
     </div>
 </AuthenticatedLayout>
 
