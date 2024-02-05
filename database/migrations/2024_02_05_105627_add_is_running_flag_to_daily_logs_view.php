8b261c815b0c4eaf47682570f4d8287f11b944e8
EmilienKopp <emilien.kopp@gmail.com>
2024-02-06 02:21:51 +0900

    migrations: added is_running flag to daily logs

---
+++ database/migrations/2024_02_05_105627_add_is_running_flag_to_daily_logs_view.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement('DROP VIEW IF EXISTS daily_project_durations');
+        DB::statement('CREATE OR REPLACE VIEW daily_logs AS
+            select
+                tl.user_id,
+                u.name as user_name,
+                tl.project_id,
+                p.name as project_name,
+                tl.date,
+                sum(
+                    extract(
+                    epoch
+                    from
+                        coalesce(tl.out_time, current_timestamp) - tl.in_time
+                    )
+                ) as total_seconds,
+                sum(
+                    extract(
+                    epoch
+                    from
+                        coalesce(tl.out_time, current_timestamp) - tl.in_time
+                    ) / 60::numeric
+                ) as total_minutes,
+                bool_or(tl.is_running) as is_running
+            from
+                time_logs tl
+            join users u on u.id = tl.user_id
+            join projects p on p.id = tl.project_id
+            group by
+                tl.user_id,
+                tl.project_id,
+                u.name,
+                p.name,
+                tl.date;
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement('DROP VIEW IF EXISTS daily_logs');
+    }
+};
