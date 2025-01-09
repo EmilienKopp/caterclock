40b3a44d26b028d5987aea6224822475ecbed9ca
EmilienKopp <emilien.kopp@gmail.com>
2025-01-09 18:39:03 +0100

    refactor: remove unused Rate model import from RateSeeder

--- database/seeders/RateSeeder.php
+++ database/seeders/RateSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Rate;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Project;
